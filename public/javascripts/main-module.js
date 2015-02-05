2a99fa2770894bf313eaad013657c926259019f3
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-02-05 08:32:06 +0100

    Added load more posts button

--- public/javascripts/main-module.js
+++ public/javascripts/main-module.js
@@ -14,21 +14,33 @@ function Post(id, content, displayName) {
 		var app = angular.module('zaibaloApp', []);
 
 		app.controller('PostsController', ['$http', '$scope', function($http, $scope) {
+			var PAGE_SIZE = 10;
+
 			var self = this;
+			self.fromIndex = 0;
 			self.posts = [];
+			self.postsCount = 0;
 			self.newPost = {
 				author: {
 					displayName: $('#username').val()
 				}
 			};
 			
-			$http.get('/posts?sort=created_at&limit=10&from=0').
+			$http.get('/posts/count').
 				success(function(data, status, headers, config) {
-					var mappedPosts = $.map(data, function(item) {
-						return new Post(item.id, item.content, item.author.displayName);
-					});
-					self.posts = mappedPosts;
+					self.postsCount = data.count;
 				});
+			
+			self.loadPosts = function(){
+				$http.get('/posts?sort=created_at&limit=' + PAGE_SIZE + '&from=' + self.fromIndex).
+				success(function(data, status, headers, config) {
+					for (var i = 0; i < data.length; i++) {
+						var post = new Post(data[i].id, data[i].content, data[i].author.displayName);
+						self.posts.unshift(post);
+					}
+					self.fromIndex = self.fromIndex + PAGE_SIZE;
+				});
+			}
 
 			self.addPost = function(posts){
 				self.newPost.creationTimestamp = new Date().getTime();
@@ -40,7 +52,17 @@ function Post(id, content, displayName) {
 				}, 'json');
 
 				this.newPost = {};
+				self.fromIndex = self.fromIndex + 1;
 			}
+			
+			self.loadMorePosts = function(){
+				if(self.postsCount > self.posts.length){
+					self.fromIndex = self.fromIndex + PAGE_SIZE;
+					self.loadPosts();
+				}
+			}
+			
+			self.loadPosts();
 		}]);
 	}
 )();
