043e32e41240df16ca9ec3fcbcec2efe747761ea
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-03-19 13:46:45 +0100

    Moved index template from Play templates to Angular

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,18 +4,6 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-	public static void index() {
-		render();
-	}
-	
-	public static void knockout() {
-		render();
-	}
-	
-    public static void client() {
-        render();
-    }
-    
 	public static void avatar() {
 		render();
 	}
