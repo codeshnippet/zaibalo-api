5a773c4fbb7ca6907eaf33afc8051a308daa5cd1
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-03-20 11:14:42 +0100

    Fixed instagram plugin

--- public/app/js/controllers/instagram.js
+++ public/app/js/controllers/instagram.js
@@ -4,20 +4,16 @@ angular.module('myApp.controllers')
       $scope.have = [];
       $scope.orderBy = "-likes.count";
       $scope.getMore = function() {
-		var endPoint = "https://api.instagram.com/v1/tags/zaibalo/media/recent?client_id=e3c2ae0fb7904532bfa625cb0f272e99";
-		$http.get(endPoint).
-			success(function(data) {
-				for(var i=0; i<data.length; i++) {
-					if (typeof $scope.have[data[i].id]==="undefined") {
-						$scope.pics.push(data[i]) ;
-						$scope.have[data[i].id] = "1";
-					}
+		var endPoint = "https://api.instagram.com/v1/tags/zaibalo.com.ua/media/recent?client_id=e3c2ae0fb7904532bfa625cb0f272e99&callback=JSON_CALLBACK&count=12";
+		$http.jsonp(endPoint).success(function(response) {
+			for(var i=0; i<response.data.length; i++) {
+				if (typeof $scope.have[response.data[i].id]==="undefined") {
+					$scope.pics.push(response.data[i]) ;
+					$scope.have[response.data[i].id] = "1";
 				}
-			});
+			}
+       });
       };
+	  
       $scope.getMore();
-      
-        $scope.tags = [
-            'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
-        ]
     });
