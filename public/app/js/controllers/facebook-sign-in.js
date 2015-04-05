2dfdd40f756a768220c0f07c942652ada34b9318
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-04-06 00:34:13 +0200

    Fixing facebook login. 8

--- public/app/js/controllers/facebook-sign-in.js
+++ public/app/js/controllers/facebook-sign-in.js
@@ -6,13 +6,13 @@ angular.module('myApp.controllers')
 
   $scope.renderSignInButton = function() {
 
-    FB.init({
-      appId: '1413679762278869',
-      channelUrl: 'partial/facebook-channel.html',
-      status: true,
-      cookie: true,
-      xfbml: true
-    });
+    // FB.init({
+    //   appId: '1413679762278869',
+    //   channelUrl: 'partial/facebook-channel.html',
+    //   status: true,
+    //   cookie: true,
+    //   xfbml: true
+    // });
 
     FB.Event.subscribe('auth.authResponseChange', function(res) {
       if (res.status === 'connected') {
