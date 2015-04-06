efd82af492dfb253e63e474ec6642fd20062f694
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-04-06 09:54:56 +0200

    Removing unnesseary log

--- public/app/js/services/user-service.js
+++ public/app/js/services/user-service.js
@@ -74,7 +74,6 @@ angular.module('myApp.services')
     FB.getLoginStatus(function(response) {
         if (response && response.status === 'connected') {
           FB.logout(function(response) {
-            console.log('FB logout.');
           });
         }
     });
