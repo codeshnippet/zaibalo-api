4020029ed2470764174d2a6d53ef92081f1fecda
Vitalii Kravets <vitalii.kravets@gmail.com>
2015-07-14 23:11:58 +0200

    Fixed zero comments posts

--- public/app/js/services/posts-service.js
+++ public/app/js/services/posts-service.js
@@ -75,8 +75,19 @@ angular.module('myApp.services')
         }
       }).
       success(function(data, status, headers, config) {
-          callback(data);
+          callback(fix(data));
       });
+  }
+
+  function fix(postsResource){
+    for (var i = 0; i < postsResource._embedded.posts.length; i++) {
+      if(!postsResource._embedded.posts[i]._embedded){
+        postsResource._embedded.posts[i]._embedded = {
+          comments :[]
+        };
+      }
+    }
+    return postsResource;
   }
 
   function buildPostsCountUrl(type, param){
