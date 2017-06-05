3be07d439ab5b570617990dca5fa0e43cefccb80
Vitalii Kravets <vitalii.kravets@gmail.com>
2017-06-05 18:42:54 +0200

    Fix security warning message

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -36,6 +36,7 @@ var app = angular.module('zabalo-web', ['zabalo-web.filters', 'zabalo-web.servic
 }])
 
 .config(function ($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy('sanitize');
     $translateProvider.useStaticFilesLoader({
       prefix: 'messages/locale-',
       suffix: '.json'
